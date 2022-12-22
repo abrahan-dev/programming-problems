503f033f0519859eb73b824aadfde9794fa11e7e
Abrahán Mesa <ab.mesa@icloud.com>
2022-12-22 11:57:43 +0100

    fizzbuzz feature: on extra fizz or buzz for each 3 or 5

--- katas-typescript/problems/fizzbuzz.ts
+++ katas-typescript/problems/fizzbuzz.ts
@@ -3,13 +3,19 @@
 export function fizzbuzz(number: number) {
     const isFizz = number % 3 === 0
     const isBuzz = number % 5 === 0
+    const numberOfFizzs = (number.toString().split("3").length - 1)
+    const numberOfBuzzs = (number.toString().split("5").length - 1)
+    let result = number.toString()
 
     if (isFizz && isBuzz) {
-        return "FizzBuzz"
+        result = "FizzBuzz"
     } else if (isFizz) {
-        return "Fizz"
+        result = "Fizz"
     } else if (isBuzz) {
-        return "Buzz"
+        result = "Buzz"
     }
-    return number.toString()
+
+    return result
+        + "Fizz".repeat(numberOfFizzs)
+        + "Buzz".repeat(numberOfBuzzs)
 }
--- katas-typescript/tests/fizzbuzz.test.ts
+++ katas-typescript/tests/fizzbuzz.test.ts
@@ -6,30 +6,50 @@ describe('Fizzbuzz', () => {
         [1, "1"],
         [2, "2"],
         [4, "4"],
-    ])('convert integers to strings: %p to %p', (number: number, result: string) => {
-        expect(fizzbuzz(number)).toBe(result);
-    });
+    ])('convert integers to strings: %p to %p',
+        (number: number, result: string) => {
+            expect(fizzbuzz(number)).toBe(result);
+        });
 
     it.each([
-        [3, "Fizz"],
         [6, "Fizz"],
         [9, "Fizz"],
         [12, "Fizz"],
-    ])('convert multiples of 3 to "Fizz": %p to %p', (number: number, result: string) => {
-        expect(fizzbuzz(number)).toBe(result);
-    });
+    ])('convert multiples of 3 to "Fizz": %p to %p',
+        (number: number, result: string) => {
+            expect(fizzbuzz(number)).toBe(result);
+        });
 
     it.each([
-        [5, "Buzz"],
         [10, "Buzz"],
-    ])('convert multiples of 5 to "Buzz": %p to %p', (number: number, result: string) => {
-        expect(fizzbuzz(number)).toBe(result);
-    });
+        [100, "Buzz"],
+    ])('convert multiples of 5 to "Buzz": %p to %p',
+        (number: number, result: string) => {
+            expect(fizzbuzz(number)).toBe(result);
+        });
 
     it.each([
-        [15, "FizzBuzz"],
-        [30, "FizzBuzz"],
-    ])('convert multiples of 3 and 5 to "FizzBuzz": %p to %p', (number: number, result: string) => {
-        expect(fizzbuzz(number)).toBe(result);
-    });
+        [3, "FizzFizz"],
+        [33, "FizzFizzFizz"],
+    ])('convert multiples of 3 to "Fizz" + n times "Fizz" for each "3" in the number: %p to %p',
+        (number: number, result: string) => {
+            expect(fizzbuzz(number)).toBe(result);
+        });
+
+    it.each([
+        [55, "BuzzBuzzBuzz"],
+        [175, "BuzzBuzz"],
+    ])('convert multiples of 5 to "Buzz" + n times "Buzz" for each "5" in the number: %p to %p',
+        (number: number, result: string) => {
+            expect(fizzbuzz(number)).toBe(result);
+        });
+
+    it.each([
+        [15, "FizzBuzzBuzz"],
+        [30, "FizzBuzzFizz"],
+        [375, "FizzBuzzFizzBuzz"],
+    ])('convert multiples of 3 and 5 to "FizzBuzz" + n times "Fizz" for each "3" + n times "Buzz" for each "5": %p to %p',
+        (number: number, result: string) => {
+            expect(fizzbuzz(number)).toBe(result);
+        });
 });
